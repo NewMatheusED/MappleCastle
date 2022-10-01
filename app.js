77d9e363e42b1666809a3f6151a7f28aa7fdaf5c
NewMatheusED <matheus081105@gmail.com>
2022-10-01 12:49:48 -0300

    .

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 $(function() {
+
+    $('ul.desktop li a, ul.mobile li a').click(function(){
+        var href = $(this).attr('href'); 
+
+        $('html,body').animate({
+            scrollTop:$(href).offset().top - 100
+        });
+    });
+
+    $('.logo a').click(function(){
+
+        $('html,body').animate({
+            scrollTop: 0
+        });
+    });
+
     $(window).on('scroll', function() {
         let coords = $(window).scrollTop()
 
-        console.log(coords)
+        console.log(coords + 'px Debug')
 
         if(coords > 0) {
             $('header').addClass('show')
         }else{
             $('header').removeClass('show')
         }
+
+        $('.chamada1').css({
+            'opacity': 1 * (1 - coords / $(window).height())
+        })
     })
 
     $('header .container .menuMobile svg').click(() => {
